9fd83422c4bf5586a8276b9bcea948a402694b6c
fnkg <andreygovorunn@gmail.com>
2025-05-22 15:49:30 +0300

    update scripts

---
+++ src/cardcash_sql/card+cash BS April.sql
@@ -0,0 +1,42 @@
+select id,
+       to_char(date, 'YYYYMMDD')                                                as date,
+       float4(amount * case when operation = 'refund' then -1 else 1 end) / 100 as sum,
+       case
+         when t1.type = 'merchant-acquiring'  then 'card'
+         when t1.type = 'raiffeisen_terminal' then 'card'
+         when t1.type = 'cash'                then 'cash'
+       end                                                                      as type,
+       case
+         when le.abbreviation = 'ЧБС' then '28cd7fa6-1c65-11ef-a230-00155d59dd05'
+       end                                                                      as store_uuid,
+       case
+         when le.abbreviation = 'ЧБС' then '0b9f6f47-d30c-11ef-a23b-00155d59dd05'
+       end                                                                      as company_uuid
+  
+  from (select fp.payment_id              as id,
+               fp.method::json ->> 'type' as type,
+               fp.amount                  as amount,
+               fpp.legal_entity_id        as le_id,
+               fp.created_at              as date,
+               fp.status                  as status,
+               fp.operation               as operation
+          
+          from finance.payment                  fp
+               left join finance.payment_method fpp on fpp.payment_method_id = fp.payment_method_id
+               left join rendered_service_view  rsv on rsv.rendered_service_id = any (fp.rendered_service_ids)
+               left join service                s
+                           on rsv.value::json ->> 'serviceId' = text(s.service_id) and s.category != '1CProduct'
+       ) as                   t1
+       
+       left join legal_entity le on le.legal_entity_id = t1.le_id
+  
+  where date >= '2025-04-01'
+    and date < '2025-05-01'
+    and t1.status in ('completed', 'receipt_failed', 'waiting_for_receipt')
+    and t1.operation in ('payment', 'account_replenishment', 'refund')
+    and le.abbreviation in ('ЧБС')
+    and t1.type in ('merchant-acquiring', 'raiffeisen_terminal', 'cash')
+  
+  group by id, date, sum, t1.type, store_uuid, company_uuid
+  
+  order by date
---
+++ src/cardcash_sql/card+cash MC April.sql
@@ -0,0 +1,42 @@
+select id,
+       to_char(date, 'YYYYMMDD')                                                as date,
+       float4(amount * case when operation = 'refund' then -1 else 1 end) / 100 as sum,
+       case
+         when t1.type = 'merchant-acquiring'  then 'card'
+         when t1.type = 'raiffeisen_terminal' then 'card'
+         when t1.type = 'cash'                then 'cash'
+       end                                                                      as type,
+       case
+         when le.abbreviation = 'Клиники Чайка' then 'e884414e-345f-11e2-9222-f23cea8074d9'
+       end                                                                      as store_uuid,
+       case
+         when le.abbreviation = 'Клиники Чайка' then '4d0460ff-a0cb-11e2-9494-91acf06830ea'
+       end                                                                      as company_uuid
+  
+  from (select fp.payment_id              as id,
+               fp.method::json ->> 'type' as type,
+               fp.amount                  as amount,
+               fpp.legal_entity_id        as le_id,
+               fp.created_at              as date,
+               fp.status                  as status,
+               fp.operation               as operation
+          
+          from finance.payment                  fp
+               left join finance.payment_method fpp on fpp.payment_method_id = fp.payment_method_id
+               left join rendered_service_view  rsv on rsv.rendered_service_id = any (fp.rendered_service_ids)
+               left join service                s
+                           on rsv.value::json ->> 'serviceId' = text(s.service_id) and s.category != '1CProduct'
+       ) as                   t1
+       
+       left join legal_entity le on le.legal_entity_id = t1.le_id
+  
+  where date >= '2025-04-01'
+    and date < '2025-05-01'
+    and t1.status in ('completed', 'receipt_failed', 'waiting_for_receipt')
+    and t1.operation in ('payment', 'account_replenishment', 'refund')
+    and le.abbreviation in ('Клиники Чайка')
+    and t1.type in ('merchant-acquiring', 'raiffeisen_terminal', 'cash')
+  
+  group by id, date, sum, t1.type, store_uuid, company_uuid
+  
+  order by date
